1aabdda071758b489d8c3a7e5b20a6d733d5281e
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-27 08:05:26 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  );
+function App() {
+  return <div className="App">Template React</div>;
 }
+
+export default App;
